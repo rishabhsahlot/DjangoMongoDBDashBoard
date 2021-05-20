d3d8f023da7482df9f9b7c41806b9ae410538d4a
Rishabh Sahlot <rishabhsahlot.rs@gmail.com>
2021-05-20 02:34:06 -0400

    commit protect

--- WSDashBoard/WSDashBoard/settings.py
+++ WSDashBoard/WSDashBoard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@s(&&of5yuibtb$xua95a24zwfl9yylt9_2d@r72(9rizdubzh'
+SECRET_KEY = "<Insert Secret Key>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
